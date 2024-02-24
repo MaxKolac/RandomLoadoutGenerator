0c29239c8e4a3afbd401aceb966146a58b9c0b34
MaxKolac <maksymilian.kolacinski@gmail.com>
2024-02-24 20:47:43 +0100

    Changed how LoadoutCombo is compared/equated

--- src/Models/LoadoutCombination.cs
+++ src/Models/LoadoutCombination.cs
@@ -1,7 +1,8 @@
 ﻿namespace RandomLoadoutGenerator.Models;
 
 /// <summary>
-/// Describes which class can equip a certain weapon, and in which slot. 
+/// Describes which class can equip a certain weapon, and in which slot.<br/>
+/// Comparing two <see cref="LoadoutCombination"/> only checks for <see cref="Slot"/> and <see cref="Class"/> values. Use <see cref="Equals(object?)"/> to include <see cref="ID"/> and <see cref="Weapons"/> values.
 /// </summary>
 public class LoadoutCombination
 {
@@ -22,4 +23,32 @@ public class LoadoutCombination
     /// Accessor member - many-to-many relation. Use this to access <see cref="Weapon"/>s which can be equipped for this <see cref="LoadoutCombination"/>.
     /// </summary>
     public IList<Weapon>? Weapons { get; set; }
+
+    ///<inheritdoc/>
+    public static bool operator ==(LoadoutCombination left, LoadoutCombination right)
+    {
+        return left.Class == right.Class && left.Slot == right.Slot;
+    }
+
+    ///<inheritdoc/>
+    public static bool operator !=(LoadoutCombination left, LoadoutCombination right)
+    { 
+        return !(left == right);
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj is not LoadoutCombination)
+            return false;
+        
+        var objAfterCast = obj as LoadoutCombination;
+        return this == objAfterCast! && ID == objAfterCast!.ID && Weapons == objAfterCast.Weapons;
+    }
+    
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
 }
